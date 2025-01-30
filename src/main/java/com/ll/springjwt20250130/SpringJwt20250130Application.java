d78c9092996da9afd4174aeb688a795a8f1b1264
Okman-0920 <nohoksun94@gmail.com>
2025-01-30 20:28:16 +0900

    settings 1

---
+++ src/main/java/com/ll/springjwt20250130/SpringJwt20250130Application.java
@@ -0,0 +1,13 @@
+package com.ll.springjwt20250130;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwt20250130Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwt20250130Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/springjwt20250130/SpringJwt20250130ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.springjwt20250130;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwt20250130ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
